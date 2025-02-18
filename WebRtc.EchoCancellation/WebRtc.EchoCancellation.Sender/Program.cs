10a671183277edd440a9ffd3e80195aa6499656f
Jorge Gonzalez <jorgeg@mill5.com>
2025-02-18 14:33:36 -0500

    Creating WebRTC client and server

---
+++ WebRtc.EchoCancellation/WebRtc.EchoCancellation.Sender/Program.cs
@@ -0,0 +1,42 @@
+using WebRtc.EchoCancellation.Server.WebRTC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("https://localhost:7164")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCors("AllowSpecificOrigin");
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapHub<SessionHub>("/webrtc-signal");
+
+// Allow cors from client url 
+
+app.Run();
---
+++ WebRtc.EchoCancellation/WebRtc.EchoCancellation.Sender/WebRTC/SessionHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebRtc.EchoCancellation.Server.WebRTC
+{
+    public class SessionHub : Hub
+    {
+        public async Task SendOffer(string offer)
+        {
+            await Clients.Others.SendAsync("ReceiveOffer", offer);
+        }
+
+        public async Task SendAnswer(string answer)
+        {
+            await Clients.Others.SendAsync("ReceiveAnswer", answer);
+        }
+
+        public async Task SendIceCandidate(string candidate)
+        {
+            await Clients.Others.SendAsync("ReceiveIceCandidate", candidate);
+        }
+    }
+}
